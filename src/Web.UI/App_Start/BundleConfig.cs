939c67c6ee36869680139061ce371a64b5697c1b
Justin Adkins <justin.adkins@outlook.com>
2016-02-06 00:33:11 -0500

    Added basic css, js. Added admin theme as well

--- src/Web.UI/App_Start/BundleConfig.cs
+++ src/Web.UI/App_Start/BundleConfig.cs
@@ -6,7 +6,44 @@ namespace eSIS.Web.UI
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            BundleTable.EnableOptimizations = true;
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/respond.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                        "~/Scripts/kendo/kendo.all.min.js",
+                        //"~/Scripts/kendo/kendo.timezones.min.js", // uncomment if using the Scheduler
+                        "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/cssadmin").Include(
+                      "~/Content/admin/bootstrap.css",
+                      "~/Content/admin/neon-core.css",
+                      "~/Content/admin/neon-forms.css",
+                      "~/Content/admin/site.css",
+                      "~/Content/admin/entypo.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+                "~/Content/kendo/kendo.common.min.css",
+                "~/Content/kendo/kendo.default.min.css"));
+
+            bundles.IgnoreList.Clear();
         }
     }
 }
