19841720545918b9ce0467743b38868a9ce2f2e3
Justin Adkins <justin.adkins@outlook.com>
2016-02-15 14:30:44 -0500

    Various UI changes

---
+++ src/Core.UI/MVC/Aui/AuiField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace eSIS.Core.UI.MVC.Aui
+{
+    public static partial class AuiHelpers
+    {
+        public static MvcHtmlString EditorTextBoxFor<TModel, TField>(this HtmlHelper<TModel> html, Expression<Func<TModel, TField>> expression)
+        {
+            var fieldGroup = new TagBuilder("div");
+            fieldGroup.AddCssClass("field-group");
+
+            return new MvcHtmlString(fieldGroup.ToString());
+        }
+    }
+}
