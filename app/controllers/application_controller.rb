5817831b13a65e7f18559ae3fb831a1e1d1f7bde
Justin Adkins <justin.adkins@mythcoders.com>
2016-07-17 20:00:19 -0400

    Basic school management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   layout 'base'
 
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
   before_action :check_user_profile
+  before_action :get_districts
 
+  def get_districts
+    @schools = School.select('id, name, short_name')
+    @districts = District.select('id, name, short_name')
+  end
 
   def check_user_profile
     if user_signed_in?
--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,16 +1,51 @@
 class SchoolsController < ApplicationController
   def index
-    if District.any?
-      flash[:system_alert] = 'No districts have been configured!'
-      @model = {}
-    else
-      @model = School.all
-    end
+    @model = School.includes(:district).all
   end
 
   def show
+    @model = School.find(params[:id])
   end
 
   def new
+    @model = School.new
+    @districts = District.select('id, name, short_name')
+  end
+
+  def create
+    @model = School.new(school_params)
+
+    if @model.save
+      redirect_to @model
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @model = School.find(params[:id])
+  end
+
+  def update
+    @model = School.find(params[:id])
+
+    if @model.update(school_params)
+      redirect_to @model
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @model = School.find(params[:id])
+    @model.destroy
+
+    redirect_to school_path
+  end
+
+  private
+
+  def school_params
+    params.require(:model).permit(:id, :name, :short_name, :district_id, :address, :city, :state, :zip_code, :phone_number, :principals_name)
   end
 end
