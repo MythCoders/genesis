bfb8d9f9c6aee80816af18f1d21f6aa093587688
Justin Adkins <justin.adkins@mythcoders.com>
2016-10-10 00:05:33 -0400

    Updated more sections to pure bootstrap

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -3,7 +3,7 @@ module SettingsHelper
     tabs = [{:name => 'grades', :partial => 'settings/grades', :label => 'Grades'},
             {:name => 'enrollcodes', :partial => 'settings/enrollcodes', :label => 'Enrollment Codes'},
             {:name => 'markscales', :partial => 'settings/markscales', :label => 'Report Card Mark Scales'},
-            {:name => 'system', :partial => 'settings/system', :label => 'System'}
+            {:name => 'system', :partial => 'settings/system', :label => 'System Settings'}
     ]
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ActiveRecord::Base
 
   belongs_to :course_category
 
+  validates :course_category_id, presence: true
+
 end
